ba98de654bdd47927deb8f06952bebfdd2215a28
Nathan Leiby <nathan.leiby@clever.com>
2017-03-03 16:06:03 -0800

    store: remove github_repo store -- unused

--- store/github_repo_store.go
+++
@@ -1,7 +0,0 @@
-package store
-
-//For "pillar" solution:
-
-//- Write is not implemented error
-//- Delete is a not implemented error
-//- Authenticate could verify the token has access to the repo? Or just set the token
