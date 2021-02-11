0a1c30126c1036d564fb896e776518acabe8241e
Ulzii Otgonbaatar <ulzii.o@clever.com>
2021-02-11 11:48:52 -0600

    make -d argument

--- scripts/tag_parameters.py
+++ scripts/tag_parameters.py
@@ -20,7 +20,7 @@ def parse_applications(file_name):
     return apps
 
 
-def aws_ssm_add_tags(env, app, secret, region, is_deploy = False):
+def aws_ssm_add_tags(env, app, secret, region, is_deploy):
     param_name = f"/{env}/{app}/{secret}"
     if is_deploy:
         param_name = f"{param_name}/current-deploy"
@@ -42,7 +42,7 @@ def aws_ssm_add_tags(env, app, secret, region, is_deploy = False):
         print(f"Could not add tags to {env}.{app}.{secret} with {e}")
 
 
-def main(applications_file):
+def main(applications_file, is_deploy):
     apps = parse_applications(applications_file)
     for env in ENVS:
         for region in REGIONS:
@@ -50,7 +50,7 @@ def main(applications_file):
             for i, app in enumerate(apps):
                 s = list_secrets(env, app)
                 for j, secret in enumerate(s):
-                    aws_ssm_add_tags(env, app, secret, region, True)
+                    aws_ssm_add_tags(env, app, secret, region, is_deploy)
                 print(f"Tagged all secrets in {env}.{app}")
 
             print(f"Done with {env} in {region}")
@@ -63,6 +63,14 @@ if __name__ == '__main__':
         action='store',
         nargs='?',
      )
+    
+    my_parser.add_argument(
+        '--deploy-param',
+        '-d',
+        action='store_true',
+        dest='is_deploy',
+    )
+    my_parser.set_defaults(is_deploy=False)
 
     args = my_parser.parse_args()
-    main(args.file_input)
+    main(args.file_input, args.is_deploy)
